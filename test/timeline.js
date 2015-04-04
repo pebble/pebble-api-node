cc2393d9ed86aa6472f403a7a8e98469a764e29c
Jack Ketcham <kcajketcham@gmail.com>
2015-04-04 13:44:02 -0700

    Make tests 100%

--- test/timeline.js
+++ test/timeline.js
@@ -75,20 +75,6 @@ describe('Timeline', function () {
       });
     });
 
-    it('should send a DELETE request to the timeline API', function(done) {
-      var timelineApi = nock('http://timeline_api', {
-        reqheaders: {
-          'X-User-Token': 'USER_TOKEN'
-        }
-      }).delete('/v1/user/pins/1234').reply(200);
-
-      timeline.deleteUserPin('USER_TOKEN', fakePin, function (err) {
-        assert.equal(err, null);
-        timelineApi.done();
-        done();
-      });
-    });
-
     describe('errors', function () {
 
       it('return an error if there is a network error', function (done) {
@@ -222,6 +208,38 @@ describe('Timeline', function () {
       }).put('/v1/shared/pins/1234').reply(200);
 
       timeline.sendSharedPin(['topic1'], fakePin, function (err) {
+        assert.equal(err, null);
+        timelineApi.done();
+        done();
+      });
+    });
+
+  });
+
+  describe('#deleteUserPin', function() {
+
+    it('should send a DELETE request to the timeline API', function(done) {
+      var timelineApi = nock('http://timeline_api', {
+        reqheaders: {
+          'X-User-Token': 'USER_TOKEN'
+        }
+      }).delete('/v1/user/pins/1234').reply(200);
+
+      timeline.deleteUserPin('USER_TOKEN', fakePin, function (err) {
+        assert.equal(err, null);
+        timelineApi.done();
+        done();
+      });
+    });
+
+    it('should convert the pin argument to a Pin object', function (done) {
+      var timelineApi = nock('http://timeline_api', {
+        reqheaders: {
+          'X-User-Token': 'USER_TOKEN'
+        }
+      }).delete('/v1/user/pins/1234').reply(200);
+
+      timeline.deleteUserPin('USER_TOKEN', fakePinData, function (err) {
         assert.equal(err, null);
         timelineApi.done();
         done();
